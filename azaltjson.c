73a3f1d63c616ce180cace90f52605506fc05e1c
murata99 <muratanaganori@realglobe.jp>
2017-11-13 19:10:38 +0900

    文字列の文字コードリスト出力およびオプション指定に対応

--- azaltjson.c
+++ azaltjson.c
@@ -116,8 +116,7 @@ azros_callback(BASEINT callback_name)
 
 /* ---------- AZ-Prolog コールバック コーディング ここまで ---------- */
 
-extern pred P2_json_term(Frame *Env);
-extern pred P3_json_term(Frame *Env);
+extern pred P3_azaltjson__json_term(Frame *Env);
 extern pred P1_azaltjson__get_prefs(Frame *Env); // 非公開API
 extern pred P1_azaltjson__set_fs(Frame *Env);    // 非公開API
 
@@ -132,9 +131,9 @@ extern int initiate_plmodule(Frame *Env);
 extern int initiate_azaltjson(Frame *Env) {
   initiate_plmodule(Env); // prologモジュール初期化（plmodule.c内部で定義）
 
-  put_bltn("json_term", 2, P2_json_term);
+  put_bltn("azaltjson__json_term", 3, P3_azaltjson__json_term);
   put_bltn("azaltjson__get_prefs", 1, P1_azaltjson__get_prefs);
-  put_bltn("azaltjson__set_fs", 1, P1_azaltjson__set_fs);
+  put_bltn("azaltjson__set_fs",    1, P1_azaltjson__set_fs);
 
   TRUE_ATOM  = PutSystemAtom(Env, "true");
   FALSE_ATOM = PutSystemAtom(Env, "false");
@@ -143,7 +142,7 @@ extern int initiate_azaltjson(Frame *Env) {
   return 1;
 };
 
-static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv) {
+static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv, int flag_fs, int flag_atom) {
   int r;
 
   switch (json_typeof(jv)) {
@@ -166,7 +165,7 @@ static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv) {
 
         MakeUndef(Env);
         TERM *val_term = next_var_cell - 1; // バリュー
-        r = make_prolog_value_from_json_value(Env, val_term, value);
+        r = make_prolog_value_from_json_value(Env, val_term, value, flag_fs, flag_atom);
         if (!r) { return r; }
 
         MakeUndef(Env);
@@ -186,16 +185,12 @@ static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv) {
 
         list_head_term = list_tail_term;
       }
-
       // リスト終端
       r = UnifyAtomE(Env, list_tail_term, ATOM_NIL);
       if (!r) { return r; }
 
-      int mode = 0; // TODO: オプション化
-      if (mode) {
+      if (!flag_fs) {
         // object -> ペアリスト格納fs複合項
-        MakeUndef(Env);
-        TERM *pair_term = next_var_cell - 1;
         r = UnifyCompTerm(Env, t, Asciz2Atom(Env, AZALTJSON__FUNCTOR_FS), 1, prefs_term);
         if (!r) { return r; }
       } else {
@@ -231,7 +226,7 @@ static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv) {
 
       MakeUndef(Env);
       TERM *val_term = next_var_cell - 1;
-      r = make_prolog_value_from_json_value(Env, val_term, ev);
+      r = make_prolog_value_from_json_value(Env, val_term, ev, flag_fs, flag_atom);
       if (!r) { return r; }
 
       MakeUndef(Env);
@@ -249,7 +244,38 @@ static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv) {
     BASEINT a;
     const char *s = json_string_value(jv);
     if (s == 0) return 0;
-    r = unify_atom(t, Asciz2Atom(Env, (char* )s));
+    if (flag_atom) {
+      // string -> アトム
+      r = unify_atom(t, Asciz2Atom(Env, (char* )s));
+      if (!r) { return r; }
+    } else {
+      // string -> 文字コードリスト格納str複合項
+      MakeUndef(Env);
+      TERM *codes_term = next_var_cell - 1;
+      TERM *list_head_term = codes_term;
+      TERM *list_tail_term = NULL;
+
+      int i = 0;
+      for (i = 0; i < strlen(s); i++) {
+        MakeUndef(Env);
+        TERM *val_term = next_var_cell - 1; // 文字コード
+        r = unify_int(val_term, (SBASEINT)s[i]);
+        if (!r) { return r; }
+
+        MakeUndef(Env);
+        list_tail_term = next_var_cell - 1; // リスト要素
+        r = UnifyCons(Env, list_head_term, val_term, list_tail_term);
+        if (!r) { return r; }
+
+        list_head_term = list_tail_term;
+      }
+      // リスト終端
+      r = UnifyAtomE(Env, list_tail_term, ATOM_NIL);
+      if (!r) { return r; }
+
+      r = UnifyCompTerm(Env, t, Asciz2Atom(Env, AZALTJSON__FUNCTOR_STR), 1, codes_term);
+      if (!r) { return r; }
+    }
     break;
   }
   case JSON_INTEGER: {
@@ -280,8 +306,8 @@ static int make_prolog_value_from_json_value(Frame* Env, TERM* t, json_t* jv) {
   return r;
 }
 
-pred P2_json_term(Frame *Env) {
-  int argc = 2;
+pred P3_azaltjson__json_term(Frame *Env) {
+  int argc = 3;
 
   json_t *jx;
   json_error_t error;
@@ -289,8 +315,46 @@ pred P2_json_term(Frame *Env) {
   char *s;
   char buf[512];
 
-  TERM *ain = PARG(argc, 0);
-  TERM *out = PARG(argc, 1);
+  TERM *opt = PARG(argc, 0);
+  TERM *ain = PARG(argc, 1);
+  TERM *out = PARG(argc, 2);
+
+  int flag_fs = 0;
+  int flag_atom = 0;
+  char flag_fs_key[] = "option2fs";
+  char flag_atom_key[] = "str2atom";
+
+  // オプション解析
+  if (UnifyAtomE(Env, opt, ATOM_NIL)) {
+    // 何もしない
+  } else if (!IsCons(opt)) {
+    YIELD(FAIL);
+  }
+  while (! IsNil(opt)) {
+    char key_str[AZ_MAX_ATOM_LENGTH] = {0};
+    if (! IsCons(opt)) { YIELD(FAIL); }
+    GetCons(opt);
+    TERM *elem_term = next_var_cell - 2;
+    opt             = next_var_cell - 1;
+
+    if (! IsCompTerm(elem_term)) { YIELD(FAIL); }
+    GetArg(elem_term, 1);
+    TERM *key_term = next_var_cell - 1; // 名前
+    GetArg(elem_term, 2);
+    TERM *val_term = next_var_cell - 1; // 値
+
+    Atom2Asciz(GetAtom(key_term), key_str);
+    if        (strcmp(key_str, flag_fs_key) == 0) {
+      // 素性構造フラグ
+      flag_fs = unify_atom(val_term, TRUE_ATOM);
+    } else if (strcmp(key_str, flag_atom_key) == 0) {
+      // アトムフラグ
+      flag_atom = unify_atom(val_term, TRUE_ATOM);
+    } else {
+      // 未知のオプションキー
+      YIELD(FAIL);
+    }
+  }
 
   len = az_term_to_cstring_length(Env, ain);
   if (len >= 512) {
@@ -303,7 +367,7 @@ pred P2_json_term(Frame *Env) {
 
   (void )az_term_to_cstring(Env, ain, s, len + 1);
 
-  jx = json_loads(s, JSON_REJECT_DUPLICATES, &error); // TODO: 追加オプション
+  jx = json_loads(s, JSON_REJECT_DUPLICATES, &error);
   if (jx == 0) {
     fprintf(stderr, "text:[%s]\n", error.text);
     fprintf(stderr, "%d:%d:%d, source:[%s]\n",
@@ -312,7 +376,7 @@ pred P2_json_term(Frame *Env) {
     YIELD(FAIL);
   }
 
-  r = make_prolog_value_from_json_value(Env, out, jx);
+  r = make_prolog_value_from_json_value(Env, out, jx, flag_fs, flag_atom);
   if (s != buf) free(s);
   if (r == 0) {
     json_object_clear(jx);
--- plmodule.pl
+++ plmodule.pl
@@ -1,4 +1,15 @@
 
+% ?- json_term(+INPUT, -TERM).
+json_term(INPUT, TERM):-
+	json_term({}, INPUT, TERM).
+
+% ?- json_term(+OPTIONS, +INPUT, -TERM).
+json_term(OPTIONS, INPUT, TERM):-
+	fstructure(OPTIONS),
+	fs_list(OPTIONS, OPTIONS_LIST),
+	azaltjson__json_term(OPTIONS_LIST, INPUT, TERM).
+
+% 非公開API
 azaltjson__makefs:-
 	azaltjson__get_prefs(PreFS),
 	fs_list(FS, PreFS),
